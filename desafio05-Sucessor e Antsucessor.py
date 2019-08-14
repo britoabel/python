0f00d83464a2517a3c0088dcb30cdf53bfc7a26d
Abel <51090320+britoabel@users.noreply.github.com>
2019-08-14 04:37:01 -0700

    Add files via upload
    ##Programa python que que o usuario introduz um numero e o programa imprime o seu sucessor e seu antessucessor

---
+++ desafio05-Sucessor e Antsucessor.py
@@ -0,0 +1,8 @@
+##um programa que le um numero e mostre o seu sucessor e seu antecessor
+
+n1 = int(input('Introduz  o numero magico: '))
+
+su = n1 + 1
+ant = n1 - 1
+
+print('O numero introduzido e {}, o seu antecessor e {}, e o seu sucessor e {}'.format(n1, ant, su))
